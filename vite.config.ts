5a4ea22fe2cfb0e1dfb4237467dbee93361085f8
luisRG-L <122104739+luisRG-L@users.noreply.github.com>
2025-03-14 15:11:23 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "https://github.com/luisRG-L/css-project-01",
 });
